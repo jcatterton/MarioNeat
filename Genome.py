d9d74c8c95c6af1d28e5fccff8891306111b0811
jcatterton <43997826+jcatterton@users.noreply.github.com>
2019-04-25 02:22:42 -0400

    Update Genome.py

--- Genome.py
+++ Genome.py
@@ -6,7 +6,10 @@ import math
 from PIL import Image, ImageDraw
 import vector_2d
 
-
+'''In a NEAT algorithm, the Genome is the network of neurons and connections which performs
+actions. Genomes may mutate by adding new neurons and connections, and adjusting the weights
+of connections. When created, genomes have only two layers (input and output), but more layers
+are created as new neurons are added.'''
 class Genome:
     def __init__(self, inputs, outputs):
         self.connection_genes = []
@@ -20,6 +23,9 @@ class Genome:
 
         local_connection_number = 0
 
+        '''The input and output layer are created manually depending on the desired inputs
+        and outputs provided'''
+        
         for i in range(inputs):
             self.node_genes.append(Node.Node(i))
             self.next_node += 1
@@ -35,6 +41,8 @@ class Genome:
         self.next_node += 1
         self.node_genes[self.bias_node].layer = 0
 
+        '''A genome is fully connected when first created'''
+        
         for i in range(inputs):
             for j in range(outputs):
                 connection_gene = ConnectionGene.ConnectionGene(self.node_genes[i], self.node_genes[inputs + j],
@@ -49,12 +57,15 @@ class Genome:
             self.connection_genes.append(connection_gene)
             local_connection_number += 1
 
+    '''Returns a neuron'''
     def get_node(self, node_number):
         for i in range(len(self.node_genes)):
             if self.node_genes[i].number == node_number:
                 return self.node_genes[i]
         return None
 
+    '''Connects all neurons as defined by the ConnectionGenes, done in order to ensure no
+    accidental loose neurons'''
     def connect_nodes(self):
         for i in range(len(self.node_genes)):
             self.node_genes[i].output_connections.clear()
@@ -62,6 +73,8 @@ class Genome:
         for i in range(len(self.connection_genes)):
             self.connection_genes[i].in_node.output_connections.append(self.connection_genes[i])
 
+    '''Send input from the input layer through all layers of the genome and returns results
+    from the output layer'''
     def feed_forward(self, input_values):
         for i in range(self.inputs):
             self.node_genes[i].output_value = input_values[i]
@@ -80,6 +93,7 @@ class Genome:
 
         return outputs
 
+    '''Adds all node genes to the network array'''
     def generate_network(self):
         self.connect_nodes()
         self.network = []
@@ -88,6 +102,7 @@ class Genome:
                 if self.node_genes[i].layer == l:
                     self.network.append(self.node_genes[i])
 
+    '''returns innovation number'''
     def get_innovation_number(self, innovation_history, in_node, out_node):
         is_new = True
         connection_innovation_number = 1000
@@ -105,6 +120,8 @@ class Genome:
                                                                           connection_innovation_number, inno_numbers))
         return connection_innovation_number
 
+    '''Adds a neuron to the genome. When a new neuron is added, an existing connection is split and the
+    new neuron is added between the two new connections. This is done to avoid extraneous neurons.'''
     def add_node(self, innovation_history):
         random_connection = random.randrange(0, len(self.connection_genes))
 
@@ -148,6 +165,7 @@ class Genome:
 
         self.connect_nodes()
 
+    '''Helper function, used to determine if the place a connection is being added is a viable option'''
     def bad_random_connections(self, random_node_1, random_node_2):
         if self.node_genes[random_node_1].layer == self.node_genes[random_node_2].layer:
             return True
@@ -155,6 +173,7 @@ class Genome:
             return True
         return False
 
+    '''Helper function, determines if there are any available spots to add a connection'''
     def fully_connected(self):
         max_connections = 0
         nodes_in_layers = []
@@ -175,6 +194,7 @@ class Genome:
             return True
         return False
 
+    '''Adds a connection between two random non-connected neurons with a random weight'''
     def add_connection(self, innovation_history):
         if self.fully_connected:
             return
@@ -201,6 +221,10 @@ class Genome:
 
         self.connect_nodes()
 
+    '''Mutates the genome. Three random numbers are generated which gives the genome an 80% chance to mutate
+    the connection weights, a 20% chance to add a new connection, and a 10% chance to add a new neuron. Three
+    separate numbers are generated in order to assure these mutations are not mutually exclusive (One mutation
+    can occur, or any combination of two, or all three, or none).'''
     def mutate(self, innovation_history):
         if len(self.connection_genes) == 0:
             self.add_connection(innovation_history)
@@ -221,6 +245,7 @@ class Genome:
         if rand_3 < 0.1:
             self.add_node(innovation_history)
 
+    '''Used to locate a matching gene in another genome'''
     @staticmethod
     def matching_gene(parent_2, innovation_number):
         for i in range(len(parent_2.connection_genes)):
@@ -228,6 +253,7 @@ class Genome:
                 return i
         return -1
 
+    '''Breeding method, takes genes from two compatable parents to create a child genome'''
     def crossover(self, parent_2):
         child = Genome(self.inputs, self.outputs, True)
         child.connection_genes.clear()
@@ -269,6 +295,7 @@ class Genome:
         child.connect_nodes()
         return child
 
+    '''Copies a genome for breeding'''
     def copy(self):
         copy = Genome(self.inputs, self.outputs)
         for i in range(len(self.node_genes)):
@@ -286,6 +313,7 @@ class Genome:
 
         return copy
 
+    '''Helper function used to show information on a genome'''
     def print_genome(self):
         print("Print genome: ")
         print("Layers: ", self.layers)
