babe4e9822bfaabb26f76b3ea46c37a4a9385e5b
jcatterton <43997826+jcatterton@users.noreply.github.com>
2019-04-25 02:13:01 -0400

    Update ConnectionGene.py

--- ConnectionGene.py
+++ ConnectionGene.py
@@ -1,7 +1,10 @@
 import random
 import numpy as np
 
-
+'''The ConnectionGene class encompases the connections which exist between neurons
+in each genome. ConnectionGenes track the input node and output node, and each have
+an associated weight, and keep track of the global innovation.
+'''
 class ConnectionGene:
     def __init__(self, in_node, out_node, weight, innovation):
         self.in_node = in_node
@@ -10,6 +13,9 @@ class ConnectionGene:
         self.innovation = innovation
         self.enabled = True
 
+    '''The mutate function mutates the weight of the connection gene. The weight
+    may be randomized between -1 and 1, or it may be adjusted slightly.
+    '''
     def mutate(self):
         if random.uniform(0, 1) < 0.1:
             self.weight = random.uniform(-1, 1)
@@ -20,6 +26,7 @@ class ConnectionGene:
         elif self.weight < -1:
             self.weight = -1
 
+    '''Copies the ConnectionGene for breeding'''
     def copy(self, in_node, out_node):
         copy = ConnectionGene(in_node, out_node, self.weight, self.innovation)
         copy.enabled = True
